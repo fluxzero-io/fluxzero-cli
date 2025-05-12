0ff3b517d50ac5a3ba35c29ce222dc7c06bb004d
Sander de Groot <sander@degroot.dev>
2025-05-12 15:24:03 +0200

    Added basic command structure for scaffolding

---
+++ README.md
@@ -0,0 +1,29 @@
+# flux-cli 
+
+The idea behind the flux-cli is an easy way to interact with flux (and flux-cloud). 
+It can scaffold new projects, perform dependency upgrades, generate example code and manage flux cloud resources.
+
+## Compatibility
+
+The `flux-cli` is built using Kotlin and distributed as a jar.
+
+## Installation
+
+The flux-cli is not installed globally. 
+Instead, each project determines which version of the cli it uses in the `.flux/config.yaml` file.
+
+## Getting started
+
+You can scaffold a new project using the following command:
+
+```shell
+curl https://flux-capacitor/install.sh | sh 
+```
+
+## Commands
+ 
+ - project create 
+
+See the `--help` for more information.
+
+
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,47 @@
 package host.flux.cli
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import com.github.ajalt.clikt.core.*
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.switch
+import com.github.ajalt.clikt.parameters.types.choice
+import com.github.ajalt.clikt.sources.PropertiesValueSource
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+class FluxCli : CliktCommand() {
+    override fun run() = Unit
+}
+
+class Init : CliktCommand() {
+
+    init {
+        context {
+            // Load default configuration options
+            valueSources(
+                PropertiesValueSource.from("~/.flux/cli.properties"),
+                PropertiesValueSource.from(".flux/cli.properties"),
+            )
+        }
+    }
+
+    override fun help(context: Context): String = "Initialize a new Flux application"
+
+    val packageName by option("--package", help = "Name of the package (com.example.your.application)")
+    val groupName by option("--group", help = "Application group name (com.example.business)")
+    val artifactName by option("--name", help = "Application name (your-application)")
+    val language by option(help = "Which language do you prefer to use? (defaults to kotlin)")
+        .choice("kotlin", "java")
+        .default("kotlin")
+
+    val anonymousMetrics by option(help = "Allow collection of anonymous usage data by Flux").switch(
+        "--enable-usage" to "enable",
+        "--disable-usage" to "disable"
+    ).default("unspecified")
+
+    override fun run() {
+        echo (anonymousMetrics)
     }
 }
+
+fun main(args: Array<String>) = FluxCli()
+    .subcommands(Init())
+    .main(args)
